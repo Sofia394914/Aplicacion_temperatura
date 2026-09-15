232a355ba29b4189df3981e3b4d11e516e738a3c
Sofia394914 <a394914@uach.mx>
2026-09-15 03:50:30 -0600

    Update apptemp.py

--- apptemp.py
+++ apptemp.py
@@ -6,4 +6,9 @@ temperatura = st.number_input(
     "Introduce la temperatura en °C:",
     value=20
 )
-if temperatura < 10
+if temperatura < 10:
+    print("Hace frio")
+elif temperatura <=10 >=24:
+    print("La temperatura es agradable.")
+else:temperatura <=25:
+    print("Hace calor.")
